f1eded9dd8d7a945638a6a2db76c7e89a8b32f1e
noellaaaaa <nhannachi937@gmail.com>
2024-11-29 15:27:50 +0100

    first commit

---
+++ config/database.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const MONGODB_URI =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`
+
+/**
+ * Fonction pour établir la connexion à la base de données.
+ */
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('🚀🚀 La connexion à la base de données a été établie avec succès 🚀🚀');
+    } catch (error) {
+        console.error('Impossible de se connecter à la base de données:', error);
+    }
+};
+
+module.exports = { connectDB };
